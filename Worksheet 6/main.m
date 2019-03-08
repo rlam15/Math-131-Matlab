65af8af92574b342a3ae25b4376546b4d250bddf
Randy Lam <rlam27@ucmerced.edu>
2019-03-08 12:51:33 -0800

    Finish Worksheet 6

--- Worksheet 6/main.m
+++ Worksheet 6/main.m
@@ -1,20 +1,18 @@
-X = [1:100];
-Y =  (X.^5)-1; 
-x = linspace(min(X)-1, max(X)+1, 100);
-
-result = linterp(X,Y,x);
-plot(x, Y, 'ko:', x, linterp(X, Y, x), 'rx:');
+p = @(x) polyval([1 0 0 0 0 -1],x);
+X = linspace(-2,2,6);
+Y = p(X);
+x = -1.7;
+error = abs(p(x)-linterp(X,Y,x))
 
 function y = linterp(X,Y,x)
-p1 = 0;
-    for i = 1: numel(X)
-        p2 = 1;  
-        for j = 1: numel(X)
-            if j~=i %Skips the current order if j = i;
-                p2 = p2 .* (x-X(j))/(X(i)-X(j)); %add the current order to the previous
-            end
+y = 0;
+for i = 1:numel(X)
+    yy = Y(i);
+    for j = 1:numel(X)
+        if i~=j
+            yy = yy * (x-X(j))/(X(i)-X(j));
         end
-        p1 = p1 + Y(i) * p2; % Adds the order multipied by the Y coefficent.
     end
-    y = p1;
+    y = y+yy;
+end
 end
